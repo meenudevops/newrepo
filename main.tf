44847ba0f1dde66380d865809c3dc3cb4f28f083
meenudevops <meenu.meenu95@gmail.com>
2021-12-08 08:56:21 +0000

    for vm and the bucket

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+    credentials = file ("gcp-account.json")
+    project     = "gcp-terraform 0324119"
+    region      = "europe-west4"
+    zone        = "europe-west4-a"
+}
+
+resource "google_compute_instance" "default" {
+ name         = "test"
+ machine_type = "e2-micro"
+
+ boot_disk {
+  initialize_params {
+   image = "debian-cloud/debian-9"
+  }
+ }
+
+ network_interface {
+  network ="default"
+ }
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "sample-bucket1"
+  location = "europe-west4"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
